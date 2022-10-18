7b352cbf6da3c6b7ec4e2a5ff3b8246673417889
Andres Leon <personalandresll@gmail.com>
2022-10-18 17:35:41 -0500

    test finalizados version 1.0.0

---
+++ test/constants.js
@@ -0,0 +1,4 @@
+module.exports = {
+    APIKEY: 'caff69331a7a9077c9bef8fdfb2dc27d',
+    APIURL: 'https://api.openweathermap.org/'
+};
---
+++ test/localizacion.js
@@ -0,0 +1,45 @@
+//imports 
+const expect = require('chai').expect;
+const ApiService = require('./../dist').ApiService;
+
+
+//Constantes
+const constantes = require('./constants');
+const APIURL = constantes.APIURL;
+const APIKEY = constantes.APIKEY;
+
+//Lo correspondiente a los tests
+describe('Busqueda del tiempo actual por localizacion geografica', () => {
+    beforeEach(() => {
+        //HAcemos el mock
+
+        const nock = require('nock');
+        const respuesta = require('./mocks/localizacion');
+        const query = {
+            lat: 35.02,
+            lon: 139.01,
+            units: 'metric',
+            lang: 'es',
+            appid: APIKEY
+        };
+
+        nock(APIURL)
+            .get('/data/2.5/weather')
+            .query(query)
+            .reply(200, respuesta);
+
+    });
+
+    //Pruebas
+    it('Tiempo de coordenadas establecida de Tawarano (Japon)', () => {
+        const api = new ApiService(APIKEY, 'es', 'metric');
+        return api.buscarPorLocalizacionGeografica({ lat: 35.02, lon: 139.01 }).then((respuesta) => {
+            expect(typeof respuesta.id).to.equal('number');
+            expect(typeof respuesta.main.temp).to.equal('number');
+            expect(typeof respuesta.main.pressure).to.equal('number');
+            expect(typeof respuesta.main.humidity).to.equal('number');
+            expect(typeof respuesta.main.temp_min).to.equal('number');
+            expect(typeof respuesta.main.temp_max).to.equal('number');
+        })
+    })
+})
